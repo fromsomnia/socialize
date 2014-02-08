c401dd35ce41d4e70e2c26060c410287a37cd935
William Chidyausiku <wchid@stanford.edu>
2014-02-07 22:35:52 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cs147GroupWebsite::Application.config.secret_token = '0514e0a195acd8f7093eb176efff44a8f5b94291250b9c016df24d6e666947de5b1a9ecbbefcc9c025fbd57e1e282f5acd562c1d7ce52fe44b8770fdd63d6da8'
