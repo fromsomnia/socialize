a020224dff5ac9f227805fde773149ddda5eab17
Natasha <natasha@natasha-VirtualBox.(none)>
2014-02-25 00:06:51 -0800

    attempt to add validation to new account creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ActiveRecord::Base
+
+validates :last_name, :first_name, :username, :password, :presence => true
+validates :username, :uniqueness => { :message => "already in use" }
+
 	before_destroy { |user|
 		Event.destroy_all("creator = #{user.id}")
 		for event in user.events
 			event.users.delete(user)
 		end
 	}
-	
   attr_accessible :first_name, :last_name, :image_url, :description, :username, :password
   has_and_belongs_to_many :events
   has_many :friendships
   has_many :friends, through: :friendships
+
 end
