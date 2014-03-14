5b6ae484f06f16a923e28def8531b97547ed6dcc
William Chidyausiku <wchid@stanford.edu>
2014-03-13 18:35:17 -0700

    fixed notice banner

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -144,7 +144,7 @@ function initializePage() {
 	$("#plus-button").bind("click", function(event){
 		revealSearch($(this), event)
 	});
-	$("#notice-banner").delay(1000).fadeOut(3000);
+	$("#notice-banner").fadeOut(2000);
 	$(".event-info-container").each(function(){
 		var cover = $(this).find(".event-info-cover");
 		var cover_val = $(this).find(".event-info-cover").attr("class")
